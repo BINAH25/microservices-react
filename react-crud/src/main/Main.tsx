831f0eb0945ab39b8d6c38f1c03d361bce45d172
BINAH25 <visteen192@gmail.com>
2025-05-14 08:07:52 +0000

    navbar added

--- react-crud/src/main/Main.tsx
+++ react-crud/src/main/Main.tsx
@@ -1,18 +1,15 @@
 import React, { useEffect, useState } from 'react'
 import { Product } from '../interfaces/product'
-import { REACT_APP_ADMIN_ENDPOINT , REACT_APP_MAIN_ENDPOINT} from "../constant";
+import { Link } from 'react-router-dom'
+import { REACT_APP_ADMIN_ENDPOINT, REACT_APP_MAIN_ENDPOINT } from '../constant'
 
 const Main = () => {
   const [products, setProducts] = useState([] as Product[])
 
   useEffect(() => {
     ;(async () => {
-      const response = await fetch(
-        REACT_APP_ADMIN_ENDPOINT
-      )
-
+      const response = await fetch(REACT_APP_ADMIN_ENDPOINT)
       const data = await response.json()
-
       setProducts(data)
     })()
   }, [])
@@ -28,45 +25,71 @@ const Main = () => {
         if (p.id === id) {
           p.likes++
         }
-
         return p
       })
     )
   }
 
   return (
-    <main role='main'>
-      <div className='album py-5 bg-light'>
+    <>
+      {/* --- NAVBAR --- */}
+      <nav className='navbar navbar-expand-lg navbar-dark bg-dark'>
         <div className='container'>
-          <div className='row'>
-            {products.map((p: Product) => {
-              return (
-                <div className='col-md-4' key={p.id}>
-                  <div className='card mb-4 shadow-sm'>
-                    <img src={p.image} height='180' alt='product_image' />
-                    <div className='card-body'>
-                      <p className='card-text'>{p.title}</p>
-                      <div className='d-flex justify-content-between align-items-center'>
-                        <div className='btn-group'>
-                          <button
-                            type='button'
-                            className='btn btn-sm btn-outline-secondary'
-                            onClick={() => like(p.id)}
-                          >
-                            Like
-                          </button>
+        <Link className='navbar-brand px-3 py-2' to='/'>
+          ProductApp
+        </Link>
+
+          <div className='collapse navbar-collapse'>
+            <ul className='navbar-nav ml-auto'>
+              <li className='nav-item'>
+                <Link className='nav-link' to='/admin/products'>
+                  Products
+                </Link>
+              </li>
+              <li className='nav-item'>
+                <Link className='nav-link' to='/admin/products/create'>
+                  Create Product
+                </Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      {/* --- MAIN CONTENT --- */}
+      <main role='main'>
+        <div className='album py-5 bg-light'>
+          <div className='container'>
+            <div className='row'>
+              {products.map((p: Product) => {
+                return (
+                  <div className='col-md-4' key={p.id}>
+                    <div className='card mb-4 shadow-sm'>
+                      <img src={p.image} height='180' alt='product_image' />
+                      <div className='card-body'>
+                        <p className='card-text'>{p.title}</p>
+                        <div className='d-flex justify-content-between align-items-center'>
+                          <div className='btn-group'>
+                            <button
+                              type='button'
+                              className='btn btn-sm btn-outline-secondary'
+                              onClick={() => like(p.id)}
+                            >
+                              Like
+                            </button>
+                          </div>
+                          <small className='text-muted'>{p.likes} likes</small>
                         </div>
-                        <small className='text-muted'>{p.likes} likes</small>
                       </div>
                     </div>
                   </div>
-                </div>
-              )
-            })}
+                )
+              })}
+            </div>
           </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </>
   )
 }
 
