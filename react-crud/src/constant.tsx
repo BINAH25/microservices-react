76655b89d8e981d281fd3baf76826c2567e3cf09
BINAH25 <visteen192@gmail.com>
2025-05-14 14:52:00 +0000

    django url changed

--- react-crud/src/constant.tsx
+++ react-crud/src/constant.tsx
@@ -1,2 +1,2 @@
 export const REACT_APP_MAIN_ENDPOINT = "http://localhost:5000/flask/api/products"
-export const REACT_APP_ADMIN_ENDPOINT = "http://localhost:8000/api/products"
+export const REACT_APP_ADMIN_ENDPOINT = "https://djando.seyram.site/api/products"
