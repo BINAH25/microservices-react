7f4ba179b25b3c2968139c8593e72b65adb5726c
BINAH25 <visteen192@gmail.com>
2025-05-15 09:23:39 +0000

    error fixed

--- react-crud/src/constant.tsx
+++ react-crud/src/constant.tsx
@@ -1,2 +1,2 @@
 export const REACT_APP_MAIN_ENDPOINT = "https://flask.seyram.site:5000/flask/api/products"
-export const REACT_APP_ADMIN_ENDPOINT = "https://django.seyram.site:8000/api/products"
+export const REACT_APP_ADMIN_ENDPOINT = "https://django.seyram.site/api/products"
