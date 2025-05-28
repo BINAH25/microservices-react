ddb4e227fb54a398b4c1a1dd2b77e97145866087
BINAH25 <visteen192@gmail.com>
2025-05-28 10:41:35 +0000

    reverse proxy

--- react-crud/src/constant.tsx
+++ react-crud/src/constant.tsx
@@ -1,2 +1,3 @@
 export const REACT_APP_MAIN_ENDPOINT = "https://flask.seyram.site/flask/api/products"
-export const REACT_APP_ADMIN_ENDPOINT = "https://django.seyram.site/api/products"
+// export const REACT_APP_ADMIN_ENDPOINT = "https://django.seyram.site/api/products"
+export const REACT_APP_ADMIN_ENDPOINT = "/api/products"
